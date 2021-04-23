9b3e9f07ec61aacb2997c603df4b202b0fa6783a
marieemhmd <muhammad.marie@gmail.com>
2021-04-23 16:45:30 +0700

    menambahkan folder SimpleApp

---
+++ SimpleApp (MVC Tutorial)/SimpleApp/Controllers/MccDayOneController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Controllers
+{
+    public class MccDayOneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DaftarParticipant()
+        {
+            List<Participant> participants = new List<Participant>();
+            participants.Add(new Participant("Agus", "Setiawan"));
+            participants.Add(new Participant("Bernadus", "Rangga"));
+            participants.Add(new Participant("Helda", "Triyanti"));
+            participants.Add(new Participant("Mar'ie", "Muhammad"));
+            participants.Add(new Participant("Muhammad", "Zulfikar"));
+            participants.Add(new Participant("Mulia Abror", "Khairul"));
+            return View(participants);
+        }
+    }
+}
---
+++ SimpleApp (MVC Tutorial)/SimpleApp/Models/Participant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Models
+{
+    public class Participant
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Participant(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
